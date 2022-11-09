23fd2fae65bd8a9ebb50ff88c7db8506b9b942ac
Conner Charlebois <cdcharlebois@gmail.com>
2022-11-09 11:44:03 -0500

    add import/export to JSON attribute

--- src/FabricEditor.jsx
+++ src/FabricEditor.jsx
@@ -1,17 +1,41 @@
 import { createElement } from "react";
+import { fabric } from "fabric";
 
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 // import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/FabricEditor.css";
 
-export function FabricEditor({ sampleText }) {
+/*
+    See the documentation here: https://www.npmjs.com/package/fabricjs-react 
+    and here: http://fabricjs.com/docs/
+
+*/
+
+export function FabricEditor({ attrJson }) {
     const {editor, onReady} = useFabricJSEditor();
     const onAddCircle = () => {
-        editor.addCircle();
+        // editor.addCircle();
+        // same as
+        editor.canvas.add(
+            new fabric.Circle({
+                radius: 20,
+                fill: "green",
+                left: 100,
+                top: 100
+            })
+        )
+    }
+    const onExportJSON = () => {
+        attrJson.setValue(JSON.stringify(editor.canvas.toJSON()));
+    }
+    const onImportJSON = () => {
+        editor.canvas.loadFromJSON(attrJson.value);
     }
     return (
         <div>
             <button onClick={onAddCircle}>Add Circle</button>
+            <button onClick={onExportJSON}>Export JSON</button>
+            <button onClick={onImportJSON}>Import JSON</button>
             <div className="fabric-canvas-container">
                 <FabricJSCanvas className="fabric-canvas" onReady={onReady} />
             </div>
