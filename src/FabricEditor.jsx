795a367ee8c372935c0c11d0411e944790c28e20
Conner Charlebois <conner.charlebois@nc6jrk6wwd.lan>
2024-08-01 19:12:16 -0400

    use a datasource

--- src/FabricEditor.jsx
+++ src/FabricEditor.jsx
@@ -1,4 +1,4 @@
-import { createElement } from "react";
+import { createElement, useEffect, useRef } from "react";
 import { fabric } from "fabric";
 
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
@@ -11,8 +11,46 @@ import "./ui/FabricEditor.css";
 
 */
 
-export function FabricEditor({ attrJson }) {
-    const {editor, onReady} = useFabricJSEditor();
+export function FabricEditor({ attrJson, datasource, objectJSON, objectType }) {
+    const { editor, onReady } = useFabricJSEditor();
+    const setListener = useRef(false);
+    useEffect(() => {
+        if (editor && setListener.current === false && datasource.status === "available") {
+            editor.canvas.on('object:modified', (event) => {
+                // todo update the mendix object
+                const { mxid } = event.target.data;
+                const item = datasource.items.find(item => item.id === mxid)
+                // cannot edit directly in widget, need to call nanoflow to do this.
+                console.warn(`attempting to update item with id: ${item.id}. new JSON value should be: `);
+                console.warn(JSON.stringify(event.target.toJSON()));
+            });
+            setListener.current = true;
+        }
+
+    }, [editor])
+    useEffect(() => {
+        if (datasource.status === "available") {
+            datasource.items.forEach((item, i) => {
+                let canvasObj;
+                switch (objectType.get(item).value) {
+                    case "circle":
+                        canvasObj = new fabric.Circle(JSON.parse(objectJSON.get(item).value))
+                        canvasObj.data = {
+                            mxid: item.id
+                        }
+                        break;
+                    default:
+                        canvasObj = new fabric.Circle({
+                            radius: 20,
+                            fill: "blue",
+                            left: 100 + 100 * i,
+                            top: 100 + 100 * i
+                        })
+                }
+                editor.canvas.add(canvasObj);
+            })
+        }
+    }, [datasource])
     const onAddCircle = () => {
         // editor.addCircle();
         // same as
